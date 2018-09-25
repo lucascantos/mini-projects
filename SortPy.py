bce598e5d99e41d4031e179da143da7f173276b8
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2018-09-25 11:59:12 -0300

    TEste

---
+++ SortPy.py
@@ -0,0 +1,15 @@
+NO = np.array(['NO','AP'])
+SE = np.array(['SE','SP','RJ'])
+SU = np.array(['SU','PR','RS','SC'])	
+
+maes = np .genfromtxt('AP2.txt', delimiter=';')
+pcd = np.genfromtxt('AP.txt', delimiter=';')
+
+for RG in NO(:1):
+	
+#Pegar o nome da primeira cidade do Maes
+#Procurar no arquivo PCD a cidade com mesmo nome
+#Devolver linha
+#---LOOP PARA AS CIDADES
+
+#Printar arquivo final com TABS pra ficar bem espaçado
