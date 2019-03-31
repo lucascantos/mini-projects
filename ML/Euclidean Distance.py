642566c08a25a44fb31bd92a6fac6e773cf6d3cc
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-03-31 13:20:48 -0300

    ML: Aula 16 - K Near

--- ML/Euclidean Distance.py
+++ ML/Euclidean Distance.py
@@ -1,8 +1,25 @@
 from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
+style.use('fivethirtyeight')
 
-plot1 = [1,3]
-plot2 = [2,5]
+dataset = { 'k': [[1,2],[2,3],[3,1]], 'r': [[6,5],[7,7],[8,6]]}
 
-euclidean_distance = sqrt( (plot1[0] - plot2[0])**2 + (plot1[1] - plot2[1])**2 )
+new_feastrues = [5,7]
 
-print(euclidean_distance)
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0], ii[1], s=100, color=i)
+
+        plt.scatter(new_feastrues[0], new_feastrues[1])
+plt.show()
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('k is set to a value less than total voting groups!')
+
+    knnalgos
+    return vote_result
