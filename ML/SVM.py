fce50abdf94cc49b2cbf3a23b08864391a6a0fb7
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-03-31 18:46:18 -0300

    ML: Aula 28 - SVM

--- ML/SVM.py
+++ ML/SVM.py
@@ -1,28 +1,63 @@
-# Suport Vector Machine
-
-
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
-from sklearn import preprocessing, model_selection, neighbors, svm
-import pandas as pd
+style.use('ggplot')
 
-df = pd.read_csv('data/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+class Supert_Vector_Machine:
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1:'r', -1:'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1,1,1)
+
+    # Train
+    def fit (self, data):
+        self.data = data
+        # { ||w||: [w,b]
+        opt_dict = {}
+
+        transforms = [[1,1],
+                      [-1,1],
+                      [-1,-1],
+                      [1,-1]]
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      # point of expense:
+                      self.max_feature_value * 0.001]
+        # extremely expensive
+        b_range_multiple = 5
+
+        b_multiple = 5
+
+        # Corner Cutting
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            # we ca do this because convex
+            optimized = False
+            while not optimized:
+                for b in np.arange()
+                pass
 
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+    def predict(self, features):
+        # sign(x.w+b)
+        classification = np.sign(np.dot(np.array(features), self.w)+self.b)
+        return classification
 
-
-clf = svm.SVC()
-clf.fit(X_train, y_train)
-
-accuracy = clf.score(X_test, y_test)
-
-print(accuracy)
-
-example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
-
-prediction = clf.predict(example_measures)
-print(prediction)
+data_dict = {-1:np.array([[1,7],
+                          [2,8],
+                          [3,8],]),
+             1:np.array([[5,1],
+                         [6,-1],
+                         [7,3],])}
