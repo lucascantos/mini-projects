d10bafb7e6b01d8799763f6e5fa7e57f026d3ef3
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-03-31 14:01:19 -0300

    ML: Aula 18 - K Near

--- ML/Euclidean Distance.py
+++ ML/Euclidean Distance.py
@@ -1,14 +1,9 @@
 from math import sqrt
 import numpy as np
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-style.use('fivethirtyeight')
-
-dataset = { 'k': [[1,2],[2,3],[3,1]], 'r': [[6,5],[7,7],[8,6]]}
-
-new_feastrues = [5,7]
+import pandas as pd
+import random
 
 
 def k_nearest_neighbors(data, predict, k=3):
@@ -20,17 +15,35 @@ def k_nearest_neighbors(data, predict, k=3):
             euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
             distances.append([euclidean_distance, group])
     votes = [i[1] for i in sorted(distances)[:k]]
-    print( Counter(votes).most_common((1)))
     vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
 
-results = k_nearest_neighbors(dataset,new_feastrues,k=3)
-print(results)
+df = pd.read_csv('data/breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
 
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+#Splitting the data
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
 
-for i in dataset:
-    for ii in dataset[i]:
-        plt.scatter(ii[0], ii[1], s=100, color=i)
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
 
-        plt.scatter(new_feastrues[0], new_feastrues[1], color=results)
-plt.show()
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct +=1
+        total +=1
+print ('Accuracy: {}'.format(correct/total))
