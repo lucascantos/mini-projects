6517953cdab6fd856b444a7a0fa9297138d841bf
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-05-19 12:19:26 -0300

    SheetsApi Done

--- GoogleAPI/Launchpad.py
+++ GoogleAPI/Launchpad.py
@@ -3,34 +3,65 @@ from httplib2 import Http
 from oauth2client import client, tools
 from oauth2client.file import Storage
 
+def discovery_google():
+        start = 'https://developers.google.com/api-client-library/python/'
+        hue = 'https://developers.google.com/apis-explorer/#p/discovery/v1/discovery.apis.getRest'
 
 def api_key():
-    # on build, use developerKey = 
-    key = ''
-    return key
+        # on build, use developerKey = 
+        key = ''
+        return key
 
 def client_credenciais ():
-    # on build, use http = 
-    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly',
-    'https://www.googleapis.com/auth/drive.readonly']
+        # on build, use http = 
+        scopes = [
+                'https://www.googleapis.com/auth/spreadsheets',
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/presentations'
+                ]
 
-    client_path = 'GoogleAPI\client_secret.json'
-    service_path = 'GoogleAPI\LucasTest01-9b2076520c24.json'
-        
-    store = Storage('storage.json')
-    credz = store.get()
-    if not credz or credz.invalid:
-        flow = client.flow_from_clientsecrets(client_path, scopes)
-        credz = tools.run_flow(flow, store)
-    return credz.authorize(Http())
+        client_path = 'GoogleAPI/credentials/client_secret.json'
+        service_path = './credentials/service_key.json'
+
+        store = Storage('storage.json')
+        credz = store.get()
+        if not credz or credz.invalid:
+                flow = client.flow_from_clientsecrets(client_path, scopes)
+                credz = tools.run_flow(flow, store)
+        return credz.authorize(Http())
 
 # Google drive
+# simple list of files on my google drive
+def drive_google():
+        '''
+        https://developers.google.com/drive/api/v3/search-files
+        '''
 
-service = build('drive', 'v3', http=client_credenciais())
-files = service.files().list().execute().get('files', [])
-print('YEET')
-print(files)
+        drive_service = build('drive', 'v3', http=client_credenciais())
+        files = drive_service.files().list(q = "mimeType='application/vnd.google-apps.spreadsheet'")
+        files = files.execute().get('files', [])
+        for f in files:
+                print (f['name'], f['mimeType'])
 
-for f in files:
-    print (f['name'], f['mimeType'])
+# Google Sheets
+# Simple listing of a sheets data
+def sheets_google():
+        '''
+        https://developers.google.com/sheets/api/reference/rest/
+        https://developers.google.com/sheets/api/samples/
+        '''
+        my_sheet = {'id': '1Bia3UOHA8aT49dap7RinFXB3uiH5eufcp3Djf_eT8bk', 'name': 'Redecs e Mar'}
+        sheets_service = build('sheets', 'v4', http=client_credenciais())
+        sheet = sheets_service.spreadsheets().values().get(
+                spreadsheetId='1Bia3UOHA8aT49dap7RinFXB3uiH5eufcp3Djf_eT8bk',
+                range="Página1",
+                majorDimension='COLUMNS')
+        sheet = sheet.execute()
 
+        sheet_data = sheets_service.spreadsheets().get(spreadsheetId=my_sheet['id'])
+        sheet_data = sheet_data.execute()
+        print(sheet['values'])
+
+
+def slides_google():
+        pass
