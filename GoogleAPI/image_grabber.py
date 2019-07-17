0ea921f60f7100bab3e72b422d3b1d55a404ad6f
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-07-17 07:22:15 -0300

    Started image grabber

--- GoogleAPI/image_grabber.py
+++ GoogleAPI/image_grabber.py
@@ -1,7 +1,36 @@
 import urllib.request
+from datetime import datetime
+
+
+def text_split():
+    # Separar entre 
+    pass
+
+
+def tropicaltidibits():
+    model = True
+    if model:
+        pass
+
+def somar(cidade=False):
+    if cidade:
+        pass
+    else:
+        pass
+
+def inmet():
+    pass
+
+def cptec():
+    pass
+
+
+
 
 def banana():
     print("Fruta")
+
+
 
 
 x = banana
