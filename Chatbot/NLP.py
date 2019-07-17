eaed162cad8dc7015a197cc881e8af4d66431a8c
Lucas <lucas.cantos@somarmeteorologia.com.br>
2019-07-16 22:04:24 -0300

    Chatbot start

---
+++ Chatbot/NLP.py
@@ -0,0 +1,18 @@
+'''
+
+NLG
+https://en.wikipedia.org/wiki/Natural-language_generation
+https://github.com/simplenlg/simplenlg/wiki/Section-0-%E2%80%93-SimpleNLG-Tutorial
+https://www.aclweb.org/anthology/E06-2020
+
+Analise de texto:
+Verificar sentenças mais comuns (Sumarization)
+
+Importar o NTKL
+Fazer um bag of words com o maximo de texto de boletim até agora
+Logistic Regression ou Neura Netwrok
+
+Boletim de tempo severo + Pitocos
+
+
+'''
