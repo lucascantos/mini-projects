3caeb50917e478adbd19ad8dab0ade98abc99370
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-07-18 16:08:22 -0300

    Started

--- GoogleAPI/image_grabber.py
+++ GoogleAPI/image_grabber.py
@@ -1,37 +1,55 @@
 import urllib.request
 from datetime import datetime
+import re
+  
 
+# Pega variaveis do link 
+class imagem(object):
+    def __init__(self, var):
+        self.var = var
+        self.database = 'database'
+        pass
+    
+    def grab_url(self):
+        single_var = (for var in database)
+        for var in 
 
-def text_split():
-    # Separar entre 
-    pass
-
-
-def tropicaltidibits():
-    model = True
-    if model:
+    def split_time(self):
         pass
 
-def somar(cidade=False):
-    if cidade:
-        pass
-    else:
-        pass
-
-def inmet():
-    pass
-
-def cptec():
-    pass
 
 
+
+
+'''
+    def text_split(self, file_url):
+        # Pega o nome de dominio
+        self.subdomain_list = file_url.split('/')
+        for self.company in self.function_list():
+            if re.search(self.company, self.subdomain_list[2]):            
+                locals()[self.company]()                
+
+    def function_list(self):
+        l = []
+        for key, value in locals().items():
+            if callable(value) and value.__module__ == __name__:
+                l.append(key)
+        return l   
+'''
 
 
 def banana():
     print("Fruta")
 
+x = 'banana'
+locals()[x]()
+
+ano = 20
+string = 'batata_{YYYY}'
+print(string.format(YYYY=ano))
 
 
 
-x = banana
-x()
+
+
+
