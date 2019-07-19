e090e5aaaab714acbe63a13af577c78f4099b063
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-07-19 11:19:41 -0300

    first version done. Testing on the way

--- GoogleAPI/image_grabber.py
+++ GoogleAPI/image_grabber.py
@@ -1,23 +1,63 @@
 import urllib.request
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import re
   
 
 # Pega variaveis do link 
 class imagem(object):
-    def __init__(self, var):
-        self.var = var
-        self.database = 'database'
-        pass
+    def __init__(self, image_name, param=''):
+        '''
+        Classe que faz o download de uma imagem baseado no nome de uma variavel
+        image_name: nome da variavel de referencia
+        param: parametro extra opcional. (nome de cidade ou numero do mapa) (por hora, admite-se que só há 1 parametro extra)
+        '''
+        self.image_name = image_name
+        self.database = 'database_link_or_url'
+        self.today_date = datetime.utcnow
+        self.param = param
     
     def grab_url(self):
-        single_var = (for var in database)
-        for var in 
+        '''
+        Busca dentro do banco de dados a linha que contem os dados.
+        '''
 
-    def split_time(self):
-        pass
+        single_image =  (data for data in self.database)
+        for i in single_image:
+             if single_image['name'] == self.image_name:
+                '''
+                Salva a url e monta uma lista com as datas que vão ser alteradas no link
+                '''
+                self.single_image = single_image
+                self.url = single_image['file_url']
+                timedelta = re.split(",", single_image['timedelta'])
+                self.time = [self.change_time(delta) for delta in timedelta]
+                yield
 
+    def split_param(self):
+        '''
+        Busca dentro da string URL por parametro entre {}.
+        '''
+        reference_list = []
+        param_list = re.findall('\{(.*?)\}', self.url)
+        for param in param_list:
+            if re.search('%', param):
+                i = reference_list.count(param)
+                self.url = self.url.replace(param, self.time[i].strftime(param))
+                reference_list.append(param)
+            else:
+                self.url.replace(param, self.param)
 
+    def change_time(self, full_delta):
+        '''
+        função que converte o timedelt em data
+        '''
+        delta = int(full_delta[:2])
+        period = full_delta[-1]
+        keydelta = {period: delta}
+        yield self.today_date + relativedelta(keydelta)
+
+        
 
 
 
@@ -45,8 +85,17 @@ x = 'banana'
 locals()[x]()
 
 ano = 20
-string = 'batata_{YYYY}'
+string = 'batata_{YYYY},2'
 print(string.format(YYYY=ano))
+print(re.findall('\{(.*?)\}',string))
+print(re.split(',', string))
+date = datetime.now()
+print(date + relativedelta(years=-1))
+
+x = [0,1,2]
+y = (a for a in x)
+for i in y:
+    print(i)
 
 
 
