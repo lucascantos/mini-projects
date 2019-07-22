d5e2d7135807d1f405607476419882159b1f5c6b
Lucas <lucas.cantos@somarmeteorologia.com.br>
2019-07-21 23:03:07 -0300

    React Testing

--- ReactNative/sandbox.js
+++ ReactNative/sandbox.js
@@ -1,28 +1,69 @@
-const name = 'lucas'
-const last = 'cantos'
+// const sayHello = (function (){
+//     var message = 'Hello!'
+//     function sayHello(){
+//         console.log(message)
 
-const val = 42
+//     }
+//     return sayHello
+// })()
 
-const arr = [
-    'sting',
-    42,
-    function(){
-        console.log('hi')
-    }
-]
+// const counter = (function(){
+//     let count = 0
 
-arr[2]()
+//     return{
+//         inc: function() { count = count + 1},
+//         get: function() { console.log(count)}
+//     }
+// })()
 
-console. log(typeof(val))
+// counter.get()
+// counter.inc()
+// counter.get()
 
-const o = new Object()
-o.firstname = 'lucas'
-o.lastname = 'cantos'
-o.student = true
-o.greet = function(){
-    console.log('howdy!')
-}
+// const name = 'lucas'
+// const last = 'cantos'
 
-const o2 = {}
 
-console.log(o['firstname'])
+// const val = 42
+// const arr = [
+//     'sting',
+//     42,
+//     function(){
+//         console.log('hi')
+//     }
+// ]
+
+// arr[2]()
+// console. log(typeof(val))
+
+
+// const o = new Object()
+// o.firstname = 'lucas'
+// o.lastname = 'cantos'
+// o.student = true
+// o.greet = function(){
+//     console.log('howdy!')
+// }
+
+// const o2 = {}
+
+// console.log(o['firstname'])
+
+
+// function makeFunctionArray(){
+//     const arr = []
+
+//     const message = 'Hello!'
+//     for (let i = 0; i <5; i++){
+//         arr.push( 
+//             (function(x){
+//                 return function (){
+//                     console.log(x)
+//                 }
+//             })(i))
+//     return arr
+//     }
+// }
+
+// const functionArr = makeFunctionArray()
+// functionArr[0]()
