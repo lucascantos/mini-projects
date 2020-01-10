a4eaf08f1a99b7b90b7f12b3217d9215d626ffb6
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2020-01-10 16:15:32 -0300

    now working

--- geojsonBuilder/geojsonMaker.py
+++ geojsonBuilder/geojsonMaker.py
@@ -2,6 +2,8 @@
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
 import json
+import re
+from unidecode import unidecode
 
 def make_feature(coordinates, geometry="Polygon"):
     singleFeature = {
@@ -29,54 +31,136 @@ def unpack_kmz(file):
 
 
 kmz_file = '5_areas_atuacao.kmz'
-kml_file = 'output.kml'
+kml_file = 'output/output.kml'
 
-tree = ET.parse(kml_file)
-root = tree.getroot()
+def decode_kml(kml_file):
+    tree = ET.parse(kml_file)
+    root = tree.getroot()
 
-regions = root.findall('Folder')
-client_region = {
-    "type": "FeatureCollection",
-    "features": []
-}
+    regions = root.findall('Folder')
 
-for region in regions:
-    region_name = region.find('name').text
-    cities = region.findall('Document')
-    region_geometry = []
-    for city in cities:
-        city_name = region.find('name').text
-        trails = city.findall('Folder')
-        coords = city.iter("coordinates")
-        city_geometry = []
-        for coord in coords:
-            text = coord.text
-            text = text.replace('\t', '')
-            text = text.replace('\n', '')
+    # DECODE KML
+    client_data = []
+    for region in regions:
+        region_data = {
+            "properties": {},
+            "cities": []
+        }
+        region_name = region.find('name').text
+        region_data['properties']['name'] = region_name
 
-            first_coord = text.split(' ')[0]
-            first_coord = first_coord.split(',')[:2]
+        cities = region.findall('Document')
+        for city in cities:
+            city_data = {
+                "properties":{},
+                "geometry": []
+            }
 
-            lon = float(first_coord[0])
-            lat = float(first_coord[1])
+            city_name = city.find('name').text
+            city_data['properties']['name'] = city_name
 
-            first_coord = [lon, lat]
+            trails = city.findall('Folder')
+            coords = city.iter("coordinates")
+            # print(city_name)
+            for coord in coords:
+                text = coord.text
+                text = text.replace('\t', '')
+                text = text.replace('\n', '')
 
-            city_geometry.append(first_coord)
-        region_geometry.append({
-           "type": "LineString",
-           "coordinates": city_geometry
-            })
+                points = text.split(' ')[:2]
+                for i, point in enumerate(points):
+                    point = point.split(',')[:2]
+                    for j, coord in enumerate(point):
+                        point[j] = float(coord)
+                    points[i] = point
+                city_data['geometry'].append(points)
+            region_data['cities'].append(city_data)
+        client_data.append(region_data)
+    return {"regions": client_data}
 
-    region_geojson = make_feature(region_geometry, 'GeometryCollection')
-    region_geojson['properties']['name'] = region_name
-    client_region['features'].append(region_geojson)
+coprel_data = decode_kml(kml_file)
+with open('output/output.json', 'w') as f:
+    json.dump(coprel_data, f)
+
+def encode_json(regions_file):
+
+    cities_geojson_file = 'inputs/municipios.json'
+    with open(cities_geojson_file) as f:
+        cities_geojson = json.load(f)
+    
+
+    with open(regions_file) as f:
+        regions_data = json.load(f)
+
+    city_features = {
+        'label': [],
+        'geometry': []
+    }
+
+    #Grabing geometry of cities
+    for feature in cities_geojson['features']:
+
+        label = feature['properties']['Label_N']
+
+        city_features['label'].append(unidecode(label))
+        city_features['geometry'].append(feature['geometry'])
+    
+    regions_collection = {
+        "type": "FeatureCollection",
+        "features": []
+    }  
+
+    for region in regions_data['regions']:
+        city_geometries = []
+
+        print(region['properties']['name'].upper())
+        for city in region['cities']:
+            city_name = city['properties']['name']
+            print(city_name)
+
+            city_index = city_features['label'].index(unidecode(city_name))
+            city_geometry = city_features['geometry'][city_index]
+            city_geometries.append(city_geometry)
+
+            # Making the GeoJson
+        region_feature = {
+            "type": "Feature",
+            "properties":region['properties'],
+            "geometry": {
+                "type": "GeometryCollection",
+                "geometries": city_geometries
+            }}
+        regions_collection['features'].append(region_feature)
+
+        return regions_collection
+
+    # region_data.append({
+    #     "type": "LineString",
+    #     "coordinates": points
+    #     })
+    # POLYGON
+    # first_coord = text.split(' ')[0]
+    # first_coord = first_coord.split(',')[:2]
+
+    # lon = float(first_coord[0])
+    # lat = float(first_coord[1])
+
+    # first_coord = [lon, lat]
+
+    #     city_data.append(first_coord)
+    # city_data.append(city_data[0])
+    # region_data.append({
+    #    "type": "LineString",
+    #    "coordinates": city_data
+    #     })
+
+    # region_geojson = make_feature(region_data, 'GeometryCollection')
+    # region_geojson['properties']['name'] = region_name
+    # client_region['features'].append(region_geojson)
 
 
-with open('output.json', 'w') as f:
-    json.dump(client_region, f)
-    # if len(child)>0:
-    #     region = child
-    #     print(region.find('name').text)
-    #     cities = region.findall('Document')
-    #     print(cities)
+regions_file = 'output/output.json'
+regions_collection = encode_json(regions_file)
+
+with open('output/area_atuacao2.json', 'w') as f:
+    json.dump(regions_collection, f)
